905212ee954681b2f5b615ea9e33da549e61b7e1
Naomie <naomie.colombo@gmail.com>
2021-08-09 22:28:25 +0200

    Database config: add a table to handle the packages creation

--- cards/models.py
+++ cards/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Package(models.Model):
+	name = models.CharField(max_length=200)
+	url = models.URLField()
+	
+	def __str__(self):
+		return self.name
+
+
 class Card(models.Model):
 	question = models.CharField(max_length=2000)
 	answer = models.CharField(max_length=2000)
+	package = models.ForeignKey(Package, on_delete=models.CASCADE, null=True)
 	tag = models.CharField(max_length=200)
-	url = models.URLField()
 
 	def __str__(self):
 		return self.question
@@ -16,6 +24,7 @@ class Learning_history(models.Model):
 	card = models.ForeignKey(Card, on_delete=models.CASCADE) 
 	number_of_study = models.IntegerField(default=0)
 	difficulty = models.CharField(max_length=200)
+	date = models.DateTimeField(null=True)
 
 	def __str__(self):
 		return self.difficulty
--- flashcards_project/settings.py
+++ flashcards_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from CONST import S_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ew3z)*td3n8x(zaf^bmyt)@z2gweq%khtvl!_so3c=phhc!sps'
+SECRET_KEY = S_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
