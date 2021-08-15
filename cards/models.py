920d6a79b263506a6e188a1dd47325962984a00e
Naomie <naomie.colombo@gmail.com>
2021-08-15 23:29:17 +0200

    first version of the card form

--- cards/models.py
+++ cards/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.forms import ModelForm
 
 # Create your models here.
 
 class Package(models.Model):
 	name = models.CharField(max_length=200)
-	url = models.URLField()
+	url = models.URLField(null=True)#add null to try save the first project
 	
 	def __str__(self):
 		return self.name
@@ -24,7 +25,13 @@ class Learning_history(models.Model):
 	card = models.ForeignKey(Card, on_delete=models.CASCADE) 
 	number_of_study = models.IntegerField(default=0)
 	difficulty = models.CharField(max_length=200)
-	date_time = models.DateTimeField(null=True)
+	date_time = models.DateTimeField(null=True, auto_now_add=True)
 
 	def __str__(self):
 		return self.difficulty
+
+
+class CardForm(ModelForm):
+	class Meta:
+		model = Card
+		fields = ['question', 'answer', 'package', 'tag']
--- cards/views.py
+++ cards/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import CardForm, Card
 
 # Create your views here.
+
+
+
+
 
 
 
@@ -11,5 +16,53 @@ def index(request):
 
 def create(request):
     """Function to display the flashcard creation page"""
-    return render(request, 'cards/create.html')
 
+    error = False
+
+    if request.method == "POST":
+        form = CardForm(request.POST)
+        if form.is_valid():
+            question_check = form.cleaned_data["question"]
+            answer_check = form.cleaned_data["answer"]
+            package_check = form.cleaned_data["package"]
+            tag_check = form.cleaned_data["tag"]
+            new_card = Card.objects.create(question=question_check, answer=answer_check,\
+            package=package_check, tag=tag_check)
+
+    else:
+        form = CardForm()
+
+        context = {
+        'form': form,
+        }
+
+    return render(request, 'cards/create.html', context)
+"""
+ error = False
+
+    if request.method == "POST":
+        form = ConnexionForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+            else: # error message
+                error = True
+    else:
+        form = ConnexionForm()
+
+    return render(request, 'account/log_in.html', locals())
+      
+
+        try:
+            product = request.POST.get('product')
+            user = request.POST.get('user')
+            prod = op_food.objects.get(id=product)
+            user_id = User.objects.get(id=user)
+            substitute.objects.get_or_create(id_substitute=prod, user=user_id)
+        except IntegrityError:
+            error = True
+
+            """
