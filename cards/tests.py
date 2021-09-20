253fdd96f899b0ca9d65bb3ab321c5dc01a24219
Naomie <naomie.colombo@gmail.com>
2021-09-20 22:23:43 +0200

    Try CI

--- cards/tests.py
+++ cards/tests.py
@@ -7,5 +7,6 @@ class IndexPageTestCase(TestCase):
     """test that index page returns a status code 200"""
     def test_index_page(self):
         """index_page status code test"""
+        
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
