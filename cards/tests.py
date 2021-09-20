186982028c451ba6e6f563873242668de80c0200
Naomie <naomie.colombo@gmail.com>
2021-09-20 22:15:56 +0200

    Add a first test + CI

--- cards/tests.py
+++ cards/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+"""Tests views and the database models"""
+
+class IndexPageTestCase(TestCase):
+    """test that index page returns a status code 200"""
+    def test_index_page(self):
+        """index_page status code test"""
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
